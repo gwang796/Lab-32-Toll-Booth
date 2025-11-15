07c89b0cb46f6b518c2349e01145e372d5cd26ce
Guo An Wang <you@example.com>
2025-11-14 17:17:30 -0800

    Initial commit

---
+++ Lab 32: Toll Booth/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 32: Toll Booth
+//
+//  Created by Guo An Wang on 11/14/25.
+//
+#include "Car.h"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
