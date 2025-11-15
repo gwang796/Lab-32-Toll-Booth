bfb1a95a7a92094acdb98214a798c0f820f9fa97
Guo An Wang <you@example.com>
2025-11-14 17:35:04 -0800

    Milestone 1

--- Lab 32: Toll Booth/main.cpp
+++ Lab 32: Toll Booth/main.cpp
@@ -8,16 +8,19 @@
 #include <iostream>
 #include <ctime>
 #include <deque>
-const int INIT = 2;
+const int INIT = 2, WIDTH = 6;
 
 int main(int argc, const char * argv[]) {
     deque<Car> parking;
     for (int i = 0; i < INIT; i++){
-        parking.push_back(new Car);
+        parking.push_back(Car());
     }
     
-    for (int i = 0; i < parking.size(); i++) {
-        parking[i]->print();
-    }
+    cout << "Initial Que: " << endl;
+    cout << setw(WIDTH);
+    parking.front().print();
+    cout << setw(WIDTH);
+    parking.back().print();
+    
     return 0;
 }
